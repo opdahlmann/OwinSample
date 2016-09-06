3202a2012b03008e45db1904d0dd62b0addfafb6
OlePetter <OlePetter@WIN-5T8IOQ7GUU8>
2016-09-06 08:02:17 +0200

    Add project files.

---
+++ OwinSample/Service/UserService.cs
@@ -0,0 +1,25 @@
+﻿using OwinSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OwinSample.Service
+{
+    public class UserService
+    {
+        public User GetUserByCredentials(string email, string password)
+        {
+            if (email != "email@domain.com" || password != "password")
+            {
+                return null;
+            }
+            User user = new User() { Id = "1", Email = "email@domain.com", Password = "password", Name = "Ole Petter Dahlmann" };
+            if (user != null)
+            {
+                user.Password = string.Empty;
+            }
+            return user;
+        }
+    }
+}
---
+++ OwinSample/provider/OAuthAppProvider.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+using OwinSample.Models;
+using OwinSample.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace OwinSample.provider
+{
+    public class OAuthAppProvider : OAuthAuthorizationServerProvider
+    {
+        public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                var username = context.UserName;
+                var password = context.Password;
+                var userService = new UserService();
+                User user = userService.GetUserByCredentials(username, password);
+                if (user != null)
+                {
+                    var claims = new List<Claim>()
+                    {
+                        new Claim(ClaimTypes.Name, user.Name),
+                        new Claim("UserID", user.Id)
+                    };
+
+                    ClaimsIdentity oAutIdentity = new ClaimsIdentity(claims, Startup.OAuthOptions.AuthenticationType);
+                    context.Validated(new AuthenticationTicket(oAutIdentity, new AuthenticationProperties() { }));
+                }
+                else
+                {
+                    context.SetError("invalid_grant", "Error");
+                }
+            });
+        }
+
+
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            if (context.ClientId == null)
+            {
+                context.Validated();
+            }
+            return Task.FromResult<object>(null);
+        }
+    }
+}
